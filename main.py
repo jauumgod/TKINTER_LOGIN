8007b1981f9f43a6ea3612cbc33f623934055f3a
jauumgod <69704112+jauumgod@users.noreply.github.com>
2022-08-18 13:44:29 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+pagina = Tk()
+pagina.geometry('1280x720')
+pagina.config(background='blue')
+
+#----------------------------------
+lb_nome = Label(pagina, text='Nome:')
+
+
+pagina.mainloop()
