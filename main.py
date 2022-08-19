bc55f59fe81857e3b36f287794212ac37b6dc6bb
jauumgod <69704112+jauumgod@users.noreply.github.com>
2022-08-18 21:29:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,56 @@
-from tkinter import *
+from tkinter import*
+from tkinter import messagebox
+from tkinter import ttk
+import DataBaser
+import cadastro_cliente
 
-pagina = Tk()
-pagina.geometry('1280x720')
-pagina.config(background='blue')
 
+
+
+tela = Tk()
+tela.geometry("1280x720")
+tela.configure(background='#c3d1d9')
+tela.resizable(width=False, height=True)
+tela.title('Programa teste')
+
+imagem = PhotoImage(file="icons/imagemhehe.png")
+
+#cadastro_produtos_01 = tela1
+#cadastro_cliente_01 = cadastro_cliente(tela1)
+
+def SairMain():
+    tela.destroy()
+
+#---------------------------------   
+def lembrarSENHA():
+    senha = 0
 #----------------------------------
-lb_nome = Label(pagina, text='Nome:')
+#layout
+TituloLabel = Label(tela, text="ENTRAR", font='Helvetica 18 bold', bg='#c3d1d9', fg="white")
+TituloLabel.place(x=1050, y=50)
+#-----------------------------------
+
+#-----------------------------------
+UserLabel = Label(tela, text="Usuario:", bg='#c3d1d9', fg="white")
+UserLabel.place(x=1000, y=140)
+UserEntry = Entry(tela, width=30)
+UserEntry.place(x=1000, y=160)
+#------------------------------------
+PasswordLabel = Label(tela,text='Senha:', bg='#c3d1d9', fg="white")
+PasswordLabel.place(x=1000, y=180)
+PasswordEntry = Entry(tela, show="*",width=30)
+PasswordEntry.place(x=1000, y=200)
+#--------------------------------------
+BtnEntrar = ttk.Button(tela, text= 'LOGIN', width=10)
+BtnEntrar.place(x=1060, y=230)
+#--------------------------------------
+x = Checkbutton(tela, text='Lembrar Senha',bg='#c3d1d9', fg="white" )
+x.place(x=1000,y=260)
+#--------------------------------------
 
 
-pagina.mainloop()
+
+
+
+
+tela.mainloop()
